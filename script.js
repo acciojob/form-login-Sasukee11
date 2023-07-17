c9509b7cea3a40112671df4c44f1964c9e4ce55f
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-17 18:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const firstName = document.querySelector('.firstName').value;
+	const lastName = document.querySelector('.lastName').value;
+	alert(`${firstName} ${lastName}`);
 }
